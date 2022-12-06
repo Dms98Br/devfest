3ecf434e7a0503b9fba75e53d87a4c12a06076e1
Daniel Moya da Silva <danimoyasilva.dev@gmail.com>
2022-12-05 23:57:19 -0300

    utilizando geolocalização para ver a temperatura

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -24,11 +25,10 @@ class _HomeState extends State<Home> {
   var velocidadedovento;
   String? apiKey = 'b2aac28faf159bdb767a6f2207460af7';
   String? openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
-  String? cityName = "Marília";
 
-  Future getDados() async {
-    var url =
-        Uri.parse('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+  Future getWeather(String lat, String log) async {
+    var url = Uri.parse(
+        '$openWeatherMapURL?lat=-22.223351&lon=-49.908340&lang=pt_br&appid=$apiKey&units=metric');
     http.Response response = await http.get(url);
     var resultado = jsonDecode(response.body);
 
@@ -41,15 +41,67 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    final location = await Geolocator.getCurrentPosition();
+    getWeather(location.latitude.toString(), location.longitude.toString());
+    return location;
+  }
+
   @override
   void initState() {
     super.initState();
-    getDados();
+    try {
+      _determinePosition();
+    } catch (e) {
+      throw Exception(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // floatingActionButton:
+      //     Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+      //   FloatingActionButton(
+      //     child: Icon(Icons.my_location),
+      //     onPressed: () => _determinePosition(),
+      //     heroTag: null,
+      //   )
+      // ]),
       body: Column(
         children: [
           Container(
@@ -122,7 +174,7 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
-          ))
+          )),
         ],
       ),
     );
